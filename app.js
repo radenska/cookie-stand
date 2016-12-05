25c3f7dcbc43ce87cae898ec0853e6b98b279884
radenska <yana_radenska@yahoo.com>
2016-12-05 14:22:58 -0800

    methods inside obj working

--- app.js
+++ app.js
@@ -1,41 +1,48 @@
 'strict use';
 
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
 var locationPike = {
   minCustomer: 23,
   maxCustomer: 65,
   aveSale: 6.3,
   hourlyCust: [],
   custPerHour: function() {
-    return (Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1)) + this.minCustomer);
+    for (var i = 0; i < hours.length; i++) {
+      this.hourlyCust[i] = (Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1)) + this.minCustomer);
+    }
   },
   numHourlyCookies: [],
   hourlySales: function() {
-    return (this.numHourlyCookies[i] = this.custPerHour() * this.aveSale);
+    this.custPerHour();
+    for (var i = 0; i < hours.length; i++) {
+      this.numHourlyCookies[i] = Math.ceil(this.hourlyCust[i] * this.aveSale);
+    }
   },
   totalCookies: 0,
   totalDailySales: function() {
-    var total = 0;
-    for (var i = 6; i < 21; i++) {
-      total += this.numHourlyCookies[i];
+    this.hourlySales();
+    for (var i = 0; i < hours.length; i++) {
+      this.totalCookies += this.numHourlyCookies[i];
     }
-    return (this.totalCookies = total);
+  },
+  go: function() {
+    return this.totalDailySales();
   }
 };
 
-for (var i = 6; i < 21; i++) {
-  locationPike.hourlyCust[i] = locationPike.custPerHour();
-}
+locationPike.go();
 
-for (var i2 = 6; i2 < 21; i2++) {
-  locationPike.numHourlyCookies[i2] = locationPike.hourlySales();
-}
+// function displayList(obj) {
+//   var cookieList = [];
+//   for (var i = 0; i < 15; i++) {
+//
+//   }
+// }
 
-locationPike.totalCookies = locationPike.totalDailySales();
-
-
-console.log('min' + locationPike.minCustomer);
-console.log('max' + locationPike.maxCustomer);
-console.log('avesale' + locationPike.aveSale);
-console.log('hourlycust' + locationPike.hourlyCust);
-console.log('numhourlycookies' + locationPike.numHourlyCookies);
-console.log('total cookies' + locationPike.totalCookies);
+console.log('min ' + locationPike.minCustomer);
+console.log('max ' + locationPike.maxCustomer);
+console.log('avesale ' + locationPike.aveSale);
+console.log('hourlycust ' + locationPike.hourlyCust);
+console.log('numhourlycookies ' + locationPike.numHourlyCookies);
+console.log('total cookies ' + locationPike.totalCookies);
