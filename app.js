e5a2fe193f60290b3b76087a1255af1bd9fd38a6
radenska <yana_radenska@yahoo.com>
2016-12-06 14:36:59 -0800

    made the table display all the numbers, but not a table

--- app.js
+++ app.js
@@ -2,21 +2,20 @@
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 var locationNameList = ['Pike and 1st', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
-var locationIDList = ['PikeList', 'SeaTacList', 'SeattleCenterList', 'CapitolHillList', 'AlkiList'];
 var minCustomerList = [23, 3, 11, 20, 2];
 var maxCustomerList = [65, 24, 38, 38, 16];
 var aveSaleList = [6.3, 1.2, 3.7, 2.3, 4.6];
 var locationObjList = [];
 
-function Location(locationName, locationID, minCustomer, maxCustomer, aveSale) {
+function Location(locationName, minCustomer, maxCustomer, aveSale) {
   this.locationName = locationName;
-  this.locationID = locationID;
   this.minCustomer = minCustomer;
   this.maxCustomer = maxCustomer;
   this.aveSale = aveSale;
   this.hourlyCust = [];
   this.totalCookies = 0;
   this.numHourlyCookies = [];
+  this.cookieTossers = 2;
   this.custPerHour = function() {
     for (var i = 0; i < hours.length; i++) {
       this.hourlyCust.push(Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1)) + this.minCustomer);
@@ -31,26 +30,57 @@ function Location(locationName, locationID, minCustomer, maxCustomer, aveSale) {
     }
   };
 
-  this.displayStats = function() {
+  this.render = function() {
     this.hourlySales(); //when hourly is run, calling totalDailySales assures all of the attributes of the object obtain data
-    var locName = document.getElementById(this.locationID);
-    var h1El = document.createElement('h1');
-    h1El.textContent = this.locationName;
-    locName.appendChild(h1El);
-    var cookieList = document.getElementById(this.locationID);
+    var trEl = document.createElement('tr');
+    var cookieList = document.getElementById('cookieInfo');
     for (var i = 0; i < hours.length; i++) {
-      var liEl = document.createElement('li');
-      liEl.textContent = hours[i] + ': ' + this.numHourlyCookies[i] + ' cookies, ' + this.hourlyCust[i] + ' customers ';
-      cookieList.appendChild(liEl);
+      var tdEl = document.createElement('td');
+      tdEl.textContent = this.numHourlyCookies[i];
+      trEl.appendChild(tdEl);
+      cookieList.appendChild(trEl);
     }
-    locName = document.getElementById(this.locationID);
-    liEl = document.createElement('li');
-    liEl.textContent = 'total ' + this.totalCookies + ' cookies';
-    locName.appendChild(liEl);
+    // var locName = document.getElementById('cookieInfo');
+    // liEl = document.createElement('li');
+    // liEl.textContent = 'total ' + this.totalCookies + ' cookies';
+    // locName.appendChild(liEl);
   };
 }
 
-for (var i = 0; i < locationNameList.length; i++) {
-  locationObjList.push(new Location(locationNameList[i], locationIDList[i], minCustomerList[i], maxCustomerList[i], aveSaleList[i]));
-  locationObjList[i].displayStats();
+function makeHeaderRow() {
+  var cookieList = document.getElementById('cookieInfo');
+  for (var i = 0; i < locationNameList.length; i++) {
+    var trEl = document.createElement('tr');
+    var thEl = document.createElement('th');
+    thEl.textContent = locationNameList[i];
+    trEl.appendChild(thEl);
+    cookieList.appendChild(trEl);
+  }
 }
+
+makeHeaderRow();
+
+for (var i = 0; i < locationNameList.length; i++) {
+  locationObjList.push(new Location(locationNameList[i], minCustomerList[i], maxCustomerList[i], aveSaleList[i]));
+  locationObjList[i].render();
+}
+
+function makeFooterRow() {
+  var totalOfTotals = 0;
+  var cookieList = document.getElementById('cookieInfo');
+  for (var i = 0; i < locationNameList.length; i++) {
+    var trEl = document.createElement('tr');
+    var tdEl = document.createElement('td');
+    tdEl.textContent = locationObjList[i].totalCookies;
+    trEl.appendChild(tdEl);
+    cookieList.appendChild(trEl);
+    totalOfTotals += tdEl;
+  }
+
+}
+
+makeFooterRow();
+
+//function to make header row
+//for loop to render each store to make table
+//function to make a footer row
