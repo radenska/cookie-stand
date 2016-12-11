81d092185a0443ec64b045595d12674e72cba6ee
radenska <yana_radenska@yahoo.com>
2016-12-10 19:25:40 -0800

    added internal order site and working on js for it

--- store.js
+++ store.js
@@ -1,13 +1,15 @@
 'strict use'
 
 var orderForm = document.getElementById('order-form');
+var OrdersArray = [['Customer Name', 'Address', 'Credit Card Type', 'Credit Card Number', '# of Choc Chip', '# of Oat Raisin', '# of Cutters', '# of shirts']];
 
 function orderHandler(event) {
   event.preventDefault();
   var tempname = event.target.name.value;
   var tempaddress1 = event.target.address1.value
   var tempcctype = event.target.cctype.value;
-  var tempccnum = event.target.ccnum.value;
+  var tempccnumString = event.target.ccnum.value;
+  var tempccnum = parseInt(event.target.ccnum.value);
   var tempcccookie = parseInt(event.target.cccookie.value);
   var temporcookie = parseInt(event.target.orcookie.value);
   var tempcutter = parseInt(event.target.cutter.value);
@@ -16,14 +18,13 @@ function orderHandler(event) {
   if(!tempcccookie && !temporcookie && !tempcutter && !temptshirt) {
     return alert('You must order at least one item!');
   }
-  if(!parseInt(tempccnum)) {
+  if(!tempccnum) {
     return alert('Your credit card number should only contain numbers, please try again!');
   }
-  if(tempccnum.length !== 16) {
+  if(tempccnumString.length !== 16) {
     return alert('Your credit card number should have 16 digits, please try again!');
   }
-
-  alert('this thing is working');
+  OrdersArray.push(tempname, tempaddress1, tempcctype, tempccnum, tempcccookie, temporcookie, tempcutter, tempshirt);
   event.target.name.value = null;
   event.target.address1.value = null;
   event.target.cctype.value = null;
