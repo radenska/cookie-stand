d1d0d4dbe04dd09d3d3d0b1ab8ccf42261802e44
radenska <yana_radenska@yahoo.com>
2016-12-11 12:41:06 -0800

    convert non entries into zeros for items ordered

--- store.js
+++ store.js
@@ -1,33 +1,12 @@
 'strict use'
 
 var orderForm = document.getElementById('order-form');
-// var OrdersArray = ['Customer Name', 'Address', 'Credit Card Type', 'Credit Card Number', '# of Choc Chip', '# of Oat Raisin', '# of Cutters', '# of shirts'];
-// var OrdersTable = document.getElementById('orders');
-// var el2;
-// var elContent = '';
-// var el;
-//
-// function appendEl(elContent) { //creates and appends an element, relies on global variables
-//   el = document.createElement('td');
-//   el.textContent = elContent;
-//   el2.appendChild(el);
-//   OrdersTable.appendChild(el2);
-// }
-//
-// function makeTable() {
-//   for (var i = OrdersArray.length; i < OrdersArray.length; i++) {
-//     el2 = document.createElement('tr');
-//     for(var j = localStorage.pending.length; j < localStorage.OrdersArray.length; j++) {
-//       elContent = localStorage.pending[j];
-//       appendEl(elContent);
-//     }
-//   }
-// }
+
 
 function orderHandler(event) {
   event.preventDefault();
   var tempname = event.target.name.value;
-  var tempaddress1 = event.target.address1.value
+  var tempaddress1 = (event.target.address1.value).replace(',',' ');
   var tempcctype = event.target.cctype.value;
   var tempccnumString = event.target.ccnum.value;
   var tempccnum = parseInt(event.target.ccnum.value);
@@ -45,10 +24,23 @@ function orderHandler(event) {
   if(tempccnumString.length !== 16) {
     return alert('Your credit card number should have 16 digits, please try again!');
   }
+  if(isNaN(tempcccookie)) {
+    tempcccookie = 0;
+  }
+  if(isNaN(temporcookie)) {
+    temporcookie = 0;
+  }
+  if(isNaN(tempcutter)) {
+    tempcutter = 0;
+  }
+  if(isNaN(temptshirt)) {
+    temptshirt = 0;
+  }
+
   var newOrder = [tempname, tempaddress1, tempcctype, tempccnum, tempcccookie, temporcookie, tempcutter, temptshirt];
   var prevOrders = localStorage.getItem('order');
   if (prevOrders === null) {
-    localStorage.setItem('order',newOrder);
+    localStorage.setItem('order', newOrder);
   } else {
     localStorage.setItem('order', prevOrders + ',' + newOrder);
   }
@@ -63,10 +55,6 @@ function orderHandler(event) {
   event.target.tshirt.value = null;
 }
 
-// function storageHandler(storageHandler) {
-//   alert('testing!');
-//   makeTable();
-// }
 
 orderForm.addEventListener('submit', orderHandler);
 
